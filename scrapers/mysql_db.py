9680ca6834b69865f75dd5db0b90dfb58b58d6ff
coderchemivan <igomezs1502@comunidad.unam.mx>
2022-11-02 16:49:39 -0600

    Find country codes not existing in countries table

--- scrapers/mysql_db.py
+++ scrapers/mysql_db.py
@@ -13,27 +13,26 @@ class MysqlDB():
     def insertar_registro(self,tabla,diccionario):
         cur = self.conn.cursor()
         if tabla =='world_trade_':
-            insertar_registro = "INSERT INTO world_trade_ (reporter_country,year,section,SA_2,SA_4, imp_exp,partner_code,tradevalue,tradequantity,quantity_unit,netweight,grossweight) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            insertar_registro = "INSERT INTO world_trade_ (reporter_country,year,section,SA_4, imp_exp,partner_code,tradevalue,fobvalue,tradequantity,quantity_unit,netweight) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             cur.execute(insertar_registro, (diccionario['reporter_country'],
                                             diccionario['year'],
                                             diccionario['section'],
-                                            diccionario['SA_2'],
                                             diccionario['SA_4'],
                                             diccionario['imp_exp'],
                                             diccionario['partner_code'],
                                             diccionario['tradevalue'],
+                                            diccionario['fobvalue'],
                                             diccionario['tradequantity'],
                                             diccionario['quantity_unit'],
                                             diccionario['netweight'],
-                                            diccionario['grossweight']))
+                                            ))
         else:
             
-            insertar_registro = "INSERT INTO world_trade (reporter_country,year,period,section,SA_2,SA_4, imp_exp,partner_code,tradevalue,tradequantity,altquantity,netweight,grossweight) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            insertar_registro = "INSERT INTO world_trade (reporter_country,year,period,section,SA_4, imp_exp,partner_code,tradevalue,tradequantity,altquantity,netweight,grossweight) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             cur.execute(insertar_registro, (diccionario['reporter_country'],
                                             diccionario['year'],
                                             diccionario['period'],
                                             diccionario['section'],
-                                            diccionario['SA_2'],
                                             diccionario['SA_4'],
                                             diccionario['imp_exp'],
                                             diccionario['partner_code'],
@@ -86,8 +85,26 @@ class MysqlDB():
         cur.close()
         return rows
     
+    def verificar_partner_code_in_countriesTable(self):
+        cur = self.conn.cursor()
+        cur.execute("SELECT distinct(partner_code_)from world_trade_;")
+        rows = cur.fetchall()
+        rows,= list(zip(*rows))
+        self.conn.commit()
+        
+        for row in rows:
+            cur.execute("SELECT name FROM countries WHERE partner_code_ = %s", (row,))
+            name = cur.fetchone()
+            if name is None:
+                print(row)
+                #cur.execute("INSERT INTO countries (partner_code_) VALUES (%s)", (row,))
+        cur.close()  
+        return rows
+    
 
 #probando
-
+# c = MysqlDB()
+# m = c.verificar_partner_code_in_countriesTable()
+# print(m)
 
 #c.insertar_registro({'bilateral_dos_num': 1, 'bilateral_cuatro_num': 1020, 'imp_exp': 2, 'country': 'United States of America', 'año_2017': '682613', 'año_2018': '756178', 'año_2019': '824127', 'año_2020': '883174', 'año_2021': '485121'})
