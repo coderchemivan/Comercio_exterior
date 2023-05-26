fc9723c31ed693687ca7cf83c634bb6963ba77c3
chemivan <ivan_gmz16@hotmail.com>
2023-05-25 20:58:15 -0600

    Change files name

--- app/prueba.py
+++ app/prueba.py
@@ -1,5 +1,6 @@
 
 from data_processing.data_processing import Data
 
-df = df_inicial_ = Data('world_trade_',fuente_datos='mysql',year=[2015,2016,2017,2018,2019,2020,2021]).read_data()
+
+df = df_inicial_ = Data('world_trade_',fuente_datos='csv',year=[2015,2016,2017,2018,2019,2020,2021]).read_data()
 print(df)
